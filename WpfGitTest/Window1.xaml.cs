0372eb5351ba8867c869dc5139e3a94888845b95
malkhut <luca.stefanelli@gmail.com>
2019-05-21 11:56:58 +0200

    Add files via upload

---
+++ WpfGitTest/Window1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfGitTest
+{
+    /// <summary>
+    /// Logica di interazione per Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        int i = 0;
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+    }
+}
